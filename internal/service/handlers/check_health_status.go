90dca835c93173a04172e49626194817557ba4d6
Maksym Hrynenko <mg06062003@gmail.com>
2023-03-27 17:31:07 +0300

    add healthcheckers

---
+++ internal/service/handlers/check_health_status.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"net/http"
+
+	"gitlab.com/distributed_lab/acs/orchestrator/internal/service/helpers"
+	"gitlab.com/distributed_lab/ape"
+)
+
+func CheckHealthStatus(w http.ResponseWriter, r *http.Request) {
+	if err := helpers.RawDB(r).Ping(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		ape.Render(w, http.StatusInternalServerError)
+		return
+	}
+
+	if !helpers.Subscriber(r).IsConnected() {
+		w.WriteHeader(http.StatusInternalServerError)
+		ape.Render(w, http.StatusInternalServerError)
+		return
+	}
+
+	if !helpers.Publisher(r).IsConnected() {
+		w.WriteHeader(http.StatusInternalServerError)
+		ape.Render(w, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	ape.Render(w, http.StatusOK)
+}
--- internal/service/helpers/ctx.go
+++ internal/service/helpers/ctx.go
@@ -2,8 +2,10 @@ package helpers
 
 import (
 	"context"
+	"database/sql"
 	"net/http"
 
+	"github.com/ThreeDotsLabs/watermill-amqp/v2/pkg/amqp"
 	"gitlab.com/distributed_lab/acs/orchestrator/internal/data"
 	"gitlab.com/distributed_lab/acs/orchestrator/internal/sender"
 
@@ -17,6 +19,9 @@ const (
 	modulesQCtxKey
 	requestsQCtxKey
 	senderCtxKey
+	rawDBCtxKey
+	publisherCtxKey
+	subscriberCtxKey
 )
 
 func CtxLog(entry *logan.Entry) func(context.Context) context.Context {
@@ -58,4 +63,37 @@ func CtxSender(entry *sender.Sender) func(context.Context) context.Context {
 
 func Sender(r *http.Request) *sender.Sender {
 	return r.Context().Value(senderCtxKey).(*sender.Sender)
+}
+
+func CtxRawDB(entry *sql.DB) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		return context.WithValue(ctx, rawDBCtxKey, entry)
+	}
+
+}
+
+func RawDB(r *http.Request) *sql.DB {
+	return r.Context().Value(rawDBCtxKey).(*sql.DB)
+}
+
+func CtxPublisher(entry *amqp.Publisher) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		return context.WithValue(ctx, publisherCtxKey, entry)
+	}
+
+}
+
+func Publisher(r *http.Request) *amqp.Publisher {
+	return r.Context().Value(publisherCtxKey).(*amqp.Publisher)
+}
+
+func CtxSubscriber(entry *amqp.Subscriber) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		return context.WithValue(ctx, subscriberCtxKey, entry)
+	}
+
+}
+
+func Subscriber(r *http.Request) *amqp.Subscriber {
+	return r.Context().Value(subscriberCtxKey).(*amqp.Subscriber)
 }
